c300180fe29df9be473aad96d7026c12055a2dec
Mastershivam7 <shivamkushwah97522@gmail.com>
2024-08-02 17:07:41 +0530

    File Added

---
+++ Rock_paper_scissor_game.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Rock_paper_scissor_game {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Welcome to Rock-Paper-Scissor");
+        System.out.println("1 for Rock \n 2 for Paper \n 3 for Scissor");
+        int UserInput = sc.nextInt();
+        int ComputerInput = random.nextInt(3+1);
+        if (UserInput == ComputerInput){
+            System.out.println("tie");
+        }
+        else if (ComputerInput == 1 && UserInput == 3 ||
+                ComputerInput == 2 && UserInput == 1 ||
+                ComputerInput == 3 && UserInput == 2)
+        {
+            System.out.println("Computer Wins");
+        }
+        else if (UserInput == 1 && ComputerInput == 3 ||
+                UserInput == 2 && ComputerInput == 1 ||
+                UserInput == 3 && ComputerInput == 2 )
+
+        {
+            System.out.println("human Wins");
+        }
+
+    }
+}
